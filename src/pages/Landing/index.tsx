f7524a58a8c48efa4e8b889fdd3a0b47ee246d73
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-03 16:51:09 -0300

    ROtas criadas

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -21,11 +21,11 @@ function Landing() {
           className="hero-image"
         />
         <div className="buttons-container">
-          <a href="" className="study">
+          <a href="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
           </a>
-          <a href="" className="give-classes">
+          <a href="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar aulas
           </a>
