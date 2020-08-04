5ff7e0d599f8e754725086dcd6035c4370aab07a
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-03 22:12:37 -0300

    Estilização do TeacherItem versão desktop

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './styles.css';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
@@ -24,34 +24,10 @@ function TeacherList() {
         </form>
       </PageHeader>
       <main>
-        <article className="teacher-item">
-          <header>
-            <img
-              src="https://avatars0.githubusercontent.com/u/11641279?s=460&u=0cf37608d66c4a30c53de48f0c65df6f1606aeca&v=4"
-              alt=""
-            />
-            <div>
-              <strong>Simone Lopes</strong>
-              <span>Química</span>
-            </div>
-          </header>
-          <p>
-            Entusiasta das melhores tecnologias de química avançada. <br />
-            <br /> Apaixonado por explodir coisas em laboratório e por mudar a
-            vida das pessoas através de experiências. Mais de 200.000 pessoas já
-            passaram por uma das minhas explosões.
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$80,00</strong>
-            </p>
-            <button>
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
