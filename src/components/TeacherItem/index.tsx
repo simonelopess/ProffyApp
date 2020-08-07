9d40107e8c28f6989b8ee32ef2aac8cf1d1ab73b
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-06 21:10:14 -0300

    Buscar método de busca no form study

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
-function TeacherItem() {
+
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars0.githubusercontent.com/u/11641279?s=460&u=0cf37608d66c4a30c53de48f0c65df6f1606aeca&v=4"
-          alt=""
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Simone Lopes</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada. <br />
-        <br /> Apaixonado por explodir coisas em laboratório e por mudar a vida
-        das pessoas através de experiências. Mais de 200.000 pessoas já passaram
-        por uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$80,00</strong>
+          <strong>{teacher.cost}</strong>
         </p>
         <button>
           <img src={whatsappIcon} alt="Whatsapp" />
@@ -33,6 +39,6 @@ function TeacherItem() {
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
